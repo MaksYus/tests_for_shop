7dfedf01ac8a3fafdade50056edf3ba9b76ca2de
maksim.iusuf <maksim.iusuf@office.tevian.ru>
2025-05-19 15:36:54 +0500

    fix test_create_category

--- tests/API/test_categories.py
+++ tests/API/test_categories.py
@@ -1,5 +1,8 @@
 import pytest
 from contextlib import nullcontext as does_not_raise
+from uuid import uuid4
+import sys
+import random
 
 from models.models import Category
 
@@ -9,34 +12,36 @@ from models.models import Category
 class TestCategories:
 
     @pytest.mark.todo
-    @pytest.mark.parametrize("name, expectation",
+    @pytest.mark.parametrize("name, status_code",
                              [
-                                 ("Электроника", does_not_raise()),
-                                 ("с пробелом", does_not_raise()),
-                                 ("""спец.символы /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/""",
-                                  does_not_raise()),
-                                 (None, does_not_raise()),
-                                 (5, does_not_raise()),
-                                 ("", does_not_raise()),
-                                 ("Электроника", does_not_raise())
+                                 (f"Электроника_{str(uuid4())}", 200),
+                                 (f"с пробелом {str(uuid4())}", 200),
+                                 (f"""спец.символы /[!@#$%^&*()_+\-=\[\];':"\\|,.<>\/?]+/{str(uuid4())}""",200),
+                                 (None, 422),
+                                 (random.randint(0,sys.maxsize), 200),
+                                 ("",400),
+                                 ("Электроника", 500)
                              ])
-    def test_create_category(self, name, expectation, category_api, get_settings_and_session_postgre):
-        with expectation:
-            response = category_api.create_new_category(data={"name": name})
+    def test_create_category(self, name, status_code, category_api, get_settings_and_session_postgre):
 
-            # проверка http ответа
-            assert response.status_code == 200
-            assert response.json()["name"] == name
+        response = category_api.create_new_category(data={"name": name})
 
-            # проверка наличия записи в базе
-            db_cat = Category(session=get_settings_and_session_postgre['session'])
-            assert db_cat.get(table_name=Category, condition=response.json()).len() > 0
-            
-            # удаляю добавленные данные
-            db_cat.delete(table_name=Category,condition={'name':name})
+        # проверка http ответа
+        assert response.status_code == status_code
+        assert response.json()["name"] == name
+
+        # проверка наличия записи в базе
+        db_cat = Category(
+            session=get_settings_and_session_postgre['session'])
+        assert db_cat.get(table_name=Category,
+                            condition=response.json()).len() > 0
+
+        # удаляю добавленные данные
+        db_cat.delete(table_name=Category, condition={'name': name})
 
     def test_read_category(self, category_api, get_settings_and_session_postgre):
-        create_response = category_api.create_new_category(data={"name": "Одежда"})
+        create_response = category_api.create_new_category(
+            data={"name": "Одежда"})
         category_id = create_response.json()["id"]
 
         response = category_api.read_category(category_id)
@@ -44,15 +49,18 @@ class TestCategories:
         assert response.json()["name"] == "Одежда"
 
         db_cat = Category(session=get_settings_and_session_postgre['session'])
-        assert db_cat.get(table_name=Category, condition=response.json())[0] == create_response.json()
+        assert db_cat.get(table_name=Category, condition=response.json())[
+            0] == create_response.json()
 
         response = category_api.read_category("-1")
         assert response.status_code == 404
         assert response.description == "Category not found"
 
-    def test_read_all_categories(self,category_api, get_settings_and_session_postgre):
-        create_response_1 = category_api.create_new_category(data={"name": "Одежда"})
-        create_response_2 = category_api.create_new_category(data={"name": "Электроника"})
+    def test_read_all_categories(self, category_api, get_settings_and_session_postgre):
+        create_response_1 = category_api.create_new_category(
+            data={"name": "Одежда"})
+        create_response_2 = category_api.create_new_category(
+            data={"name": "Электроника"})
 
         response = category_api.read_categories()
         assert response.status_code == 200
@@ -66,4 +74,3 @@ class TestCategories:
 
         assert create_response_1.json() in all_categories
         assert create_response_2.json() in all_categories
-    
