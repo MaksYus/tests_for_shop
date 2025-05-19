fdb2d155381fae9e3cbd57a2d107be15d95f1179
leopoldj <leopoldj@student.21-school.ru>
2025-05-19 14:57:44 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -1,18 +1,7 @@
 import pytest
-
 import os
 
-def get_fixtures():
-    files_list = []
-    for root, dirs,files in (os.walk("./fixtures")):
-        for file in files:
-            if '.pyc' not in file:
-                root_folder = root[root.find('fixtures'):].replace('\\','.').replace('/','.')
-                file_name = file[:len(file) -3]
-                files_list.append(f'{root_folder}.{file_name}')
-    if len(files_list) == 0:
-        raise pytest.PytestWarning('Не поддерживаемый запуск')
-    return files_list
+from helpers.helpers import get_fixtures
 
 pytest_plugins = get_fixtures()
 
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,5 +1,6 @@
 # тут лежат вспомогательные ф-ции
 import os
+import pytest
 
 def get_settings():
     settings_config={}
@@ -7,3 +8,15 @@ def get_settings():
     for key, value in spis.items():
         settings_config[key] = value
     return settings_config
+
+def get_fixtures():
+    files_list = []
+    for root, dirs,files in (os.walk("./fixtures")):
+        for file in files:
+            if '.pyc' not in file:
+                root_folder = root[root.find('fixtures'):].replace('\\','.').replace('/','.')
+                file_name = file[:len(file) -3]
+                files_list.append(f'{root_folder}.{file_name}')
+    if len(files_list) == 0:
+        raise pytest.PytestWarning('Не поддерживаемый запуск')
+    return files_list
